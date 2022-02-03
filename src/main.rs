39c7279c5023b78d3aca5bb768570b55e6584b28
syobon <syobon@vivaldi.net>
2022-02-03 16:46:08 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -11,25 +12,52 @@ fn main() {
         .expect("something went wrong reading the dictionary");
     let dictionary: Vec<String> = contents.split_whitespace().map(|x| x.to_owned()).collect();
     let mut words = dictionary.clone();
+    let mut filtering_table = dictionary;
+    filtering_table = update_filtering_table(&filtering_table, &[], &[], &[]);
 
     let mut confirmed: Vec<char> = "_____".chars().collect();
     let mut with: Vec<char> = Vec::new();
     let mut without: Vec<char> = Vec::new();
 
-    while confirmed.contains(&'_') {
+    for _ in 0..6 {
+        println!("\n==Status==");
         print!("Current: {}", confirmed.iter().cloned().collect::<String>());
-        if with.len() > 0 {
+        if !with.is_empty() {
             print!(" / with {}", with.iter().cloned().collect::<String>());
         }
-        if without.len() > 0 {
+        if !without.is_empty() {
             print!(" / without {}", without.iter().cloned().collect::<String>());
         }
         println!();
+        println!("Words remaining: {}", words.len());
 
-        let attempt = choose(&words);
+        let attempt;
+        if !filtering_table.is_empty() {
+            println!("Too many words remaining. Trying to reduce the number of words...");
+            attempt = choose(&filtering_table);
+        } else {
+            attempt = choose(&words);
+        }
+
+        if let Some(x) = words.clone().into_iter().position(|x| x == attempt) {
+            words.swap_remove(x);
+        }
+        if let Some(x) = filtering_table
+            .clone()
+            .into_iter()
+            .position(|x| x == attempt)
+        {
+            filtering_table.swap_remove(x);
+        }
+
+        println!("==========");
         println!("{}", attempt);
         let chars = attempt.chars().collect::<Vec<char>>();
         let result = read_line();
+        if result == "22222" {
+            println!("Finished!");
+            return;
+        }
         for i in 0..5 {
             match result.chars().collect::<Vec<char>>()[i] {
                 '2' => confirmed[i] = chars[i],
@@ -48,15 +76,29 @@ fn main() {
                 }
             }
         }
-        words = filter(&words, &confirmed, &with, &without);
+        for i in 0..with.len() {
+            if confirmed.contains(&with[i]) {
+                with.swap_remove(i);
+            }
+        }
+        with.sort_unstable();
+        without.sort_unstable();
+
+        words = update_words(&words, &confirmed, &with, &without);
+        filtering_table = update_filtering_table(&filtering_table, &confirmed, &with, &without);
+    }
+    println!("Failed...");
+    println!("Remaining words:");
+    for word in words {
+        println!("{}", word);
     }
 }
 
-fn filter(
-    words: &Vec<String>,
-    confirmed: &Vec<char>,
-    with: &Vec<char>,
-    without: &Vec<char>,
+fn update_words(
+    words: &[String],
+    confirmed: &[char],
+    with: &[char],
+    without: &[char],
 ) -> Vec<String> {
     let state = vec![
         confirmed[0] != '_',
@@ -70,8 +112,10 @@ fn filter(
     for word in words {
         let chars = word.chars().collect::<Vec<char>>();
         let mut is_valid = true;
-        for letter in with {
-            if !chars.contains(letter) {
+        for i in 0..5 {
+            if !state[i] || chars[i] == confirmed[i] {
+                continue;
+            } else {
                 is_valid = false;
                 break;
             }
@@ -88,12 +132,8 @@ fn filter(
         if !is_valid {
             continue;
         }
-        for i in 0..5 {
-            if !state[i] {
-                continue;
-            } else if chars[i] == confirmed[i] {
-                continue;
-            } else {
+        for letter in with {
+            if !chars.contains(letter) {
                 is_valid = false;
                 break;
             }
@@ -106,7 +146,37 @@ fn filter(
     match_words
 }
 
-fn choose(words: &Vec<String>) -> String {
+fn update_filtering_table(
+    words: &[String],
+    confirmed: &[char],
+    with: &[char],
+    without: &[char],
+) -> Vec<String> {
+    let mut match_words: Vec<String> = Vec::new();
+    for word in words {
+        let chars = word.chars().collect::<Vec<char>>();
+        let mut is_valid = true;
+        for letter in &chars {
+            if without.contains(letter) || with.contains(letter) || confirmed.contains(letter) {
+                is_valid = false;
+                break;
+            }
+        }
+        if !is_valid {
+            continue;
+        }
+        if chars.into_iter().collect::<HashSet<char>>().len() < 5 {
+            is_valid = false;
+        }
+        if is_valid {
+            match_words.push(word.to_owned());
+        }
+    }
+
+    match_words
+}
+
+fn choose(words: &[String]) -> String {
     let mut rng = rand::thread_rng();
     words[rng.gen_range(0..words.len())].to_owned()
 }
